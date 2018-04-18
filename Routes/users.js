1487c405415e1b6eed8b76c884abbdc95c1b04f1
Daniel <daniel.097@hotmail.com>
2018-04-18 16:17:42 +0100

    Update users.js

--- Routes/users.js
+++ Routes/users.js
@@ -1,99 +1,94 @@
-acvar express = require('express');
+var express = require('express');
 var router = express.Router();
-var Comment = require('../models/comments')
 var User = require('../models/users');
 var jwt = require('jsonwebtoken');
 
-
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
 });
 
-/* GET home page. */
-router.get('/feed', function(req, res, next) {
-    res.render('feed');
+router.get('/login', function(req, res, next) {
+  res.render('login.hbs', { title: 'Petal Palet - Curated Colour Palette Inspiration' });
 });
 
-/* ADD COMMENTS */
-
-router.post('/addComment', function(req, res, next) {
-    
-    comment = new Comment(req.body);
-    comment.save(function (err, savedComment) {
-        if(err)
-            throw err;
-        
-        res.json({
-            "id": savedComment._id
-        });
-    });
+router.get('/register', function(req, res, next) {
+  res.render('register.hbs', { title: 'Petal Palet - Curated Colour Palette Inspiration' });
 });
-  
 
-/* UPDATE COMMENTS */
-router.put('/updateComment/:id', function(req, res, next){
-
-    var id = req.params.id;
-    Comment.update({_id:id}, req.body, function (err) {
+router.post('/register', function(req, res, next){
+    var username = req.body.user_name;
+    var password = req.body.password;
+    // Check if account already exists
+    User.findOne({ 'user_name' :  username }, function(err, user)
+    {
         if (err)
             res.send(err);
+        // check to see if theres already a user with that email
+        if (user) {
+            res.status(401).json({
+                "status": "info",
+                "body": "Username already taken"
+            });
+        } else {
+            // If there is no user with that username create the user
+            var newUser = new User();
 
-        res.json({status : "Successfully updated the document"});
-    });
-});
+            // set the user's local credentials
+            newUser.user_name = username;
+            newUser.password = newUser.generateHash(password);
+            newUser.access_token = createJwt({user_name:username});
+            newUser.save(function(err, user) {
+                if (err)
+                    throw err;
+	     res.cookie('Authorization', 'Bearer ' + user.access_token);
+                res.json({'success' : 'account created'});
 
-/* RETURN COMMENTS */
-
-router.get('/getComments', function(req, res, next) {
-    
-    Comment.find({}, function (err, comments) {
-        if (err)
-            res.send(err);
-        res.json(comments);
-    });
-});
-
-
-/* DELETE COMMENTS */
-router.delete('/removeComment/:id', function(req, res, next){
-
-    var id = req.params.id;
-    Comment.remove({_id:id}, function (err) {
-        if (err)
-            res.send(err);
-
-        res.json({status : "successfully removed the document"});
-    });
-});
-
-/* GET feed page. */
-router.get('/feed', function(req, res, next) {
-
-    try {
-        var jwtString = req.cookies.Authorization.split(" ");
-        var profile = verifyJwt(jwtString[1]);
-        if (profile) {
-            res.render('feed');
-        }
-    }catch (err) {
-            res.json({
-                "status": "error",
-                "body": [
-                    "You are not logged in."
-                ]
             });
         }
+    });
 });
 
 /*
- Verifies a JWT
+ Creates a JWT
  */
-function verifyJwt(jwtString) {
-
-    var value = jwt.verify(jwtString, 'CSIsTheWorst');
-    return value;
+function createJwt(profile) {
+    return jwt.sign(profile, 'CSIsTheWorst', {
+        expiresIn: '10d'
+    });
 }
+
+router.post('/login', function(req, res, next){
+    var username = req.body.user_name;
+    var password = req.body.password;
+    User.findOne({'user_name': username}, function (err, user) {
+        // if there are any errors, return the error
+        if (err)
+            res.send(err);
+        // If user account found then check the password
+        if (user) {
+          // Compare passwords
+            if (user.validPassword(password)) {
+                // Success : Assign new access token for the session
+                user.access_token = createJwt({user_name: username});
+                user.save();
+                res.cookie('Authorization', 'Bearer ' + user.access_token);
+                res.json({'success' : 'loggedIn'});
+            }
+            else {
+                res.status(401).send({
+                    "status": "error",
+                    "body": "Email or password does not match"
+                });
+            }
+        }
+        else
+        {
+            res.status(401).send({
+                "status": "error",
+                "body": "Username not found"
+            });
+        } }); });
 
 
 module.exports = router;
