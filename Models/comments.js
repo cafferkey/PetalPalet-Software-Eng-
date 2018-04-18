a4c2cbf2c574c6d35d40a5ffec0eb30c41f875ff
cafferkey <seanog.cafferkey@gmail.com>
2018-04-18 16:33:33 +0100

    Update comments.js

--- Models/comments.js
+++ Models/comments.js
@@ -5,9 +5,9 @@ require('./util');
 var commentsSchema = new Schema({
     user_name: {type: String},
     comment: {type: String},
-    date_created : {type: Date, default: new Date()},
-    up_votes: {type: Number, default : 0},
-    down_votes: {type: Number, default : 0}
+    date_created: {type: Date, default: new Date()},
+    up_votes: {type: Number, default: 0},
+    down_votes: {type: Number, default: 0}
 });
 
-module.exports =mongoose.mo
+module.exports = mongoose.model('Comment', commentsSchema);
